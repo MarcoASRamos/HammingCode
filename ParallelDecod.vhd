f79ff2a210d8e672c0a7616975453d05fe3fd8e9
Marco <msramos99@ua.pt>
2021-11-25 20:06:11 +0000

    begin

---
+++ ParallelDecod.vhd
@@ -0,0 +1,11 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+entity ParallelDecod is
+port();
+end ParallelDecod;
+
+architecture Behavoral of ParallelDecod is
+begin
+
+end;
---
+++ ParallelEnc.vhd
@@ -0,0 +1,11 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+entity ParallelEnc is
+port();
+end ParallelEnc;
+
+architecture Behavoral of ParallelEnc is
+begin
+
+end;
